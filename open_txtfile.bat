eb51458024ed1387df4f871d16163de9e02693d4
SetsunaYumemukai <125370960+SetsunaYumemukai@users.noreply.github.com>
2023-05-28 09:58:03 +0900

    Add files via upload

---
+++ open_txtfile.bat
@@ -0,0 +1,5 @@
+@echo off
+
+#2個めの""はtxtファイルを格納した特定の階層、XXXXXは開きたいtxtファイルを記載
+#The second "" is the specific hierarchy where the txt file is stored, and XXXXX is the txt file you want to open.
+start "" "C:\Users\Username\desktop\XXXXX.txt"
